b9189cc7772922f80328801b8b1d5e03245b22b0
Daniel Ge <DanGe42@users.noreply.github.com>
2018-07-29 23:48:00 -0700

    Rename renumber_files -> renumber-files

--- renumber-files.rb
+++ renumber-files.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require 'pathname'
 
 input_dir = ARGV[0]
@@ -10,8 +9,17 @@ if !input_dir || !prefix || !output_dir
   abort "Usage: renumber_files.rb directory prefix out_directory"
 end
 
+def try_mkdir(path)
+  return if path.directory?
+
+  raise "Path #{path} exists but is not a directory" if path.exist?
+  path.mkdir
+end
+
 input_dir = Pathname.new(input_dir)
 output_dir = Pathname.new(output_dir)
+
+try_mkdir(output_dir)
 
 files = Dir.glob(input_dir + "*.JPG")
 digits = files.size.to_s.size
